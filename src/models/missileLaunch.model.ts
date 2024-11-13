dfa8eb216f6648719ffbfbf2ef16aec881778e82
Shalom <shlomo000@gmail.com>
2024-11-13 14:57:42 +0200

    finish IMissileLaunch schema

--- src/models/missileLaunch.model.ts
+++ src/models/missileLaunch.model.ts
@@ -1,7 +1,19 @@
 import { MissileLaunchStatusEnum } from '../enums/MissileLaunchStatusEnum'
+import { Document, model, Schema } from 'mongoose'
 
-export interface missileLaunch {
+export interface IMissileLaunch extends Document {
   rocketType: string
   timeToHit: number
   status: MissileLaunchStatusEnum
 }
+
+const missileLaunchSchema = new Schema<IMissileLaunch>({
+  rocketType: String,
+  timeToHit: Number,
+  status: {
+    type: String,
+    enum: ['Intercepted', 'Hit', 'Launched'],
+  },
+})
+
+export default model<IMissileLaunch>('MissileLaunch', missileLaunchSchema)
