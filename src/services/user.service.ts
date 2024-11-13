3eb85610cb0869ac0a29132715eff14ef09fabe8
Shalom <shlomo000@gmail.com>
2024-11-13 12:19:48 +0200

    add login and register logic

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,17 +1,45 @@
-import UserDTO from '../DTO/user.DTO'
+import { compare, hash } from 'bcrypt'
+import { UserLoginDTO, UserRegisterDTO } from '../DTO/user.DTO'
+import organizations from '../data/organizations.json'
+import userModel, { IUser } from '../models/user.model'
+import { IOrganization } from '../models/organization.model'
+import jwt  from 'jsonwebtoken'
 
-export const register = (user: UserDTO) => {
+export const registerService = async (user: UserRegisterDTO) => {
   try {
-    
+    const newUser = {
+      name: user.name,
+      password: await hash(user.password, 10),
+      orgnization: organizations.find(
+        (o) => o.name === user.orgnizationName
+      ) as IOrganization,
+    }
+
+    const dbUser = new userModel(newUser)
+    return dbUser.save()
   } catch (err) {
-    
+    throw err
   }
 }
 
-export const login = (user: UserDTO) => {
+export const loginService = async (user: UserLoginDTO) => {
   try {
-    
+    if (!user.password || !user.name)
+      throw new Error('Missing user data')
+    const dbUser = await userModel.findOne({ username: user.name }).lean()
+    if (!dbUser) throw new Error('User not found')
+    const metch = await compare(user.password, dbUser.password)
+    if (!metch) throw new Error('Wrong password')
+
+    const token = jwt.sign({
+      user_id: dbUser._id,
+      username: dbUser.password,
+      organization: dbUser.orgnization.name
+    }, process.env.JWT_SECRET || 'defualt', {
+      expiresIn: '10m'
+    })
+    return {...dbUser, token, password: '*******'}
   } catch (err) {
-    
+    throw err
   }
 }
