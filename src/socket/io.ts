a01b4ef11fcdc6df3fa7182a918ca1b2f99c1b55
Shalom <shlomo000@gmail.com>
2024-11-15 13:08:13 +0200

    modify socket configuration

--- src/socket/io.ts
+++ src/socket/io.ts
@@ -1,20 +1,35 @@
 import { Socket } from 'socket.io'
 import { IMissileLaunch } from '../models/missileLaunch.model'
 import missiles from '../data/missiles.json'
+import { OrgnizationsEnum } from '../enums/orgnizationEnum'
+import { io } from '../app'
 
 export const handelIoConnection = (socket: Socket) => {
   console.log(`User Connected: ${socket.id}`)
 
   socket.on('join_room', (roomName) => {
-    socket.join(roomName)
-    console.log(`User with ID: ${socket.id} joined room: ${roomName}`)
+    if(roomName.split(' ')[0] != 'IDF') {
+      socket.join(OrgnizationsEnum.IDFCenter)
+      socket.join(OrgnizationsEnum.IDFNorth)
+      socket.join(OrgnizationsEnum.IDFSouth)
+      socket.join(OrgnizationsEnum.IDFWestBank)
+      console.log(`User with ID: ${socket.id} joined room: terorists room`)
+    }else{
+      console.log(`User with ID: ${socket.id} joined room: ${roomName}`)
+    }
   })
 
   socket.on('missile_launch', (misileLaunch: IMissileLaunch) => {
-    misileLaunch.timeToHit = missiles.find(m=> m.name == misileLaunch.rocketType)?.speed!
-    socket.to(misileLaunch.launchTo).emit('new_launch_has_accord', misileLaunch)
-    console.log(`missile_launch to ${misileLaunch.launchTo}`)
-    socket.to(misileLaunch.launchFrom).emit('new_launch_has_accord', misileLaunch)
+    io.emit('new_launch_has_accord')
+    io.to(misileLaunch.launchTo).emit('new_launch_has_accord')
+    console.log(
+      `missile_launch to ${misileLaunch.launchTo} from ${misileLaunch.launchFrom}`
+    )
+    io.to(misileLaunch.launchFrom).emit('new_launch_has_accord')
+  })
+
+  socket.on('interception_launch', ()=>{
+    io.emit('new_launch_has_accord')
   })
 
   socket.on('disconnect', () => {
