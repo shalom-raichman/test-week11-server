9a50eb7446fd0807f19fcac164e1b330d4f3d8d2
Shalom <shlomo000@gmail.com>
2024-11-17 17:43:17 +0200

    fix the logic on missile launch

--- src/services/missiles.service.ts
+++ src/services/missiles.service.ts
@@ -46,8 +46,10 @@ export const launchMissileService = async (
     resources.amount -= 1
     dbUser.save()
     const newMissileLaunch = new missileLaunchModel(missileLaunch)
+    const dbLaunch = await newMissileLaunch.save()
     handelCountToHit(newMissileLaunch._id as string)
-    return await newMissileLaunch.save()
+
+    return dbLaunch
   } catch (err) {
     throw err
   }
@@ -57,16 +59,13 @@ export const handelCountToHit = async (_id: string) => {
   try {
     const dbLaunch = await missileLaunchModel.findById(_id)
     if (!dbLaunch) throw new Error('launch not found')
-    if (
-      dbLaunch.status == MissileLaunchStatusEnum.Hit ||
-      dbLaunch.status == MissileLaunchStatusEnum.Intercepted
-    )
-      return
-    setInterval(() => {
+    const myInterval = setInterval(() => {
+      dbLaunch.status == MissileLaunchStatusEnum.Launched &&
       dbLaunch.timeToHit > 0
         ? (dbLaunch.timeToHit -= 1)
         : (dbLaunch.status = MissileLaunchStatusEnum.Hit)
       dbLaunch.save()
+      clearInterval(myInterval)
     }, 1000)
   } catch (err) {
     throw err
@@ -89,10 +88,13 @@ export const interceptionService = async (
   interceptorType: InterceptorsEnum
 ) => {
   try {
+    console.log(interceptorType)
     const dbLaunch = await missileLaunchModel.findById(_id)
     if (!dbLaunch) throw new Error('launch not found')
     const interceptor = missiles.find((m) => m.name == interceptorType)
     if (!interceptor) throw new Error('interceptor type is not valid')
+    if(dbLaunch.status == MissileLaunchStatusEnum.Intercepted) throw new Error('missile alredy intercepted');
+    
     if (
       interceptor.intercepts.includes(dbLaunch.rocketType) &&
       interceptor.speed <= dbLaunch.timeToHit
@@ -101,8 +103,7 @@ export const interceptionService = async (
       dbLaunch.timeToHit = 0
       dbLaunch.save()
     } else {
-      throw new Error("missile allredy hit the target ):");
-      
+      throw new Error('missile allredy hit the target ):')
     }
   } catch (err) {
     throw err
