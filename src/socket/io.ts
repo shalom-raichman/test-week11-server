19026c5573a8adca1111e47c7266a1d666370dbf
Shalom <shlomo000@gmail.com>
2024-11-17 17:41:16 +0200

    remove unneccery code

--- src/socket/io.ts
+++ src/socket/io.ts
@@ -7,29 +7,12 @@ import { io } from '../app'
 export const handelIoConnection = (socket: Socket) => {
   console.log(`User Connected: ${socket.id}`)
 
-  socket.on('join_room', (roomName) => {
-    if(roomName.split(' ')[0] != 'IDF') {
-      socket.join(OrgnizationsEnum.IDFCenter)
-      socket.join(OrgnizationsEnum.IDFNorth)
-      socket.join(OrgnizationsEnum.IDFSouth)
-      socket.join(OrgnizationsEnum.IDFWestBank)
-      console.log(`User with ID: ${socket.id} joined room: terorists room`)
-    }else{
-      console.log(`User with ID: ${socket.id} joined room: ${roomName}`)
-    }
-  })
-
   socket.on('missile_launch', (misileLaunch: IMissileLaunch) => {
     io.emit('new_launch_has_accord')
-    io.to(misileLaunch.launchTo).emit('new_launch_has_accord')
-    console.log(
-      `missile_launch to ${misileLaunch.launchTo} from ${misileLaunch.launchFrom}`
-    )
-    io.to(misileLaunch.launchFrom).emit('new_launch_has_accord')
   })
 
-  socket.on('interception_launch', ()=>{
-    io.emit('new_launch_has_accord')
+  socket.on('interception_launch', (socket) => {
+    io.emit('new_launch_has_accord', { _id: (socket as any)._id })
   })
 
   socket.on('disconnect', () => {
